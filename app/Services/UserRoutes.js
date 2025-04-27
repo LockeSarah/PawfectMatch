c9fcc9d79f7705ec574ebb8c33a5fe80f5a78f18
LockeSarah <sarahlocke2001@gmail.com>
2025-04-26 22:31:47 -0400

    DONE

--- app/Services/UserRoutes.js
+++ app/Services/UserRoutes.js
@@ -84,7 +84,7 @@ async function LoginUser(username) {
     try {
         const result = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
         if (result.rows.length > 0) {
-            console.log("User found:", result.rows[0]); // Log the user data
+            // console.log("User found:", result.rows[0]);
             return result.rows[0];
         } else {
             console.log("No user found with username:", username);
